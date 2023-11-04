58515afff29e2823f39bf37182a24017bd43f484
doanhuynhduycuong1601 <doanhuynhduycuong1601@gmail.com>
2023-11-04 17:19:46 +0700

    Update
    Xóa chưa hết

--- src/main/java/org/weebook/api/controller/OrderController.java
+++ src/main/java/org/weebook/api/controller/OrderController.java
@@ -45,8 +45,5 @@ public class OrderController {
         return orderService.adminFindByStatus(status, page);
     }
 
-//    @GetMapping("/tk/by/order")
-//    public List<TKProductDto> userFindByStatus(@Param("status") String status, @Param("page") Integer page){
-//        return orderService.adminFindByStatus(status, page);
-//    }
+
 }
